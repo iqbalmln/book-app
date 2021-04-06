11ed341cf51b0935e22c826b0962933f80f9794c
Iqbal Maulana <masadepansilauu@gmail.com>
2021-04-06 11:05:23 +0700

    Create dynamic user profile and profile settings

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -16,41 +16,34 @@
 @section('content')
 <div class="header">
     <div class="avatar-image">
-        <img src="https://images.unsplash.com/flagged/photo-1556151994-b611e5ab3675?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWx8ZW58MHwyfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="">
+        <img src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDd8fG1vZGVsfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="">
         <div class="avatar-icon-x">
         <!-- <i class="cil-pencil"></i> -->
         </div>
     </div>
     <div class="avatar-caption">
-        <h1>Nyi Roro Kidul</h1>
-        <p>East Java, Indonesia</p>
+        <h1>{{ $user[0]->first_name }} {{ $user[0]->last_name }}</h1>
     </div>
 </div>
 <div class="content">
     <div class="col-flex">
         <div class="flex-item-left">
             <p>First Name</p>
-            <h5>Iqbal</h5>
+            <h5>{{ $user[0]->first_name }}</h5>
         </div>
         <div class="flex-item-right">
             <p>Last Name</p>
-            <h5>Maulana</h5>
+            <h5>{{ $user[0]->last_name }}</h5>
         </div>
     </div>
     <div class="col-flex">
         <div class="flex-item-left">
             <p>Email Address</p>
-            <h5>miqblmaulana@gmail.com</h5>
+            <h5>{{ $user[0]->email_address }}</h5>
         </div>
         <div class="flex-item-right">
             <p>Role</p>
-            <h5>Admin</h5>
-        </div>
-    </div>
-    <div class="col-flex">
-        <div class="flex-item-left">
-            <p>Password</p>
-            <h5>xxxxx</h5>
+            <h5>{{ $user[0]->role->role_name }}</h5>
         </div>
     </div>
 </div>
